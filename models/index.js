9eeb3a0b050ef5e0dec7bd8fa65642c2cd53c6be
wickhill <wickhill@gmail.com>
2024-06-10 09:32:11 -0400

    Initial commit for backend deployment

---
+++ models/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+const userSchema = require('./User');
+
+
+console.log("userSchema:", userSchema); // debug log to check the schema
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const User = require('./User');
+
+module.exports = {
+    User
+};
