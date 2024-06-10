2c0f8538acd69fc00ee3665507971124f07257f7
wickhill <wickhill@gmail.com>
2024-06-10 11:51:00 -0400

    deleted and rebuild 'colorMapping.js', case-issues were causing Netlify deployment to fail

--- frontend/src/colormapping.js
+++ frontend/src/colormapping.js
@@ -1,16 +1,16 @@
 const colorMapping = {
-        conclusion1: "bg-blue-500 hover:bg-blue-300",
-        conclusion2: "bg-purple-500 hover:bg-purple-300",
-        conclusion3: "bg-green-500 hover:bg-green-300",
-        conclusion4: "bg-green-500 hover:bg-green-300",
-        conclusion5: "bg-blue-500 hover:bg-blue-300",
-        conclusion6: "bg-purple-500 hover:bg-purple-300",
-        conclusion7: "bg-blue-500 hover:bg-blue-300",
-        conclusion8: "bg-green-500 hover:bg-green-300",
-        conclusion9: "bg-purple-500 hover:bg-purple-300",
-        conclusion10: "bg-blue-500 hover:bg-blue-300",
-        conclusion11: "bg-green-500 hover:bg-green-300",
-        conclusion12: "bg-purple-500 hover:bg-purple-300",
-    };
+    conclusion1: "bg-blue-500 hover:bg-blue-300",
+    conclusion2: "bg-purple-500 hover:bg-purple-300",
+    conclusion3: "bg-green-500 hover:bg-green-300",
+    conclusion4: "bg-green-500 hover:bg-green-300",
+    conclusion5: "bg-blue-500 hover:bg-blue-300",
+    conclusion6: "bg-purple-500 hover:bg-purple-300",
+    conclusion7: "bg-blue-500 hover:bg-blue-300",
+    conclusion8: "bg-green-500 hover:bg-green-300",
+    conclusion9: "bg-purple-500 hover:bg-purple-300",
+    conclusion10: "bg-blue-500 hover:bg-blue-300",
+    conclusion11: "bg-green-500 hover:bg-green-300",
+    conclusion12: "bg-purple-500 hover:bg-purple-300",
+};
 
 export default colorMapping
